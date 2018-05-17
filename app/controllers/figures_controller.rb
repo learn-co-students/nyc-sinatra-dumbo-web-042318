85a24d2b3c649a9e147f423e50d085dd55e225d8
Daniel Shimon <danielzshimon@gmail.com>
2018-05-17 15:31:56 -0400

    more done

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -26,8 +26,13 @@ class FiguresController < ApplicationController
 
   # create
   post '/figures' do
-    binding.pry
     figure = Figure.create(params["figure"])
+    if !params['title']['name'].empty?
+      figure.titles << Title.create(params['title'])
+    end
+    if !params['landmark']['name'].empty?
+      figure.landmarks << Landmark.create(params['landmark'])
+    end
     redirect "figures/#{figure.id}"
   end
 
