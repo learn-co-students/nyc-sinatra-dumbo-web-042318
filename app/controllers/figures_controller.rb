d91e3a485c77d3033ff32f443d8399fc62e054fa
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-17 11:15:36 -0400

    add roadmap to fig controller

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,13 +1,49 @@
 class FiguresController < ApplicationController
   set :views, Proc.new { File.join(root, "../views/figures") }
+  #index
   get '/figures' do
     @figures = Figure.all
     erb :index
   end
-
+  #show
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :show
   end
+ 
+#   new
+#   GET
+#   ‘/posts/new’
+#   displays create post form
+#
+# create
+#   POST
+#   ‘/posts’
+#   creates one blog post
+#
+# edit
+#   GET
+#   ‘/posts/:id/edit’
+#   displays edit form based on ID in the url
+#
+# edit
+#   PATCH
+#   ‘/posts/:id’
+#   edits an existing blog post based on ID in the url
+#
+# update
+#   GET
+#   ‘/posts/:id/update’
+#   displays update form blog post based on ID in the url
+#
+# update
+#   PUT
+#   ‘/posts/:id’
+#   replaces an existing blog post based on ID in the url
+#
+# delete
+#   DELETE
+#   ‘/posts/:id/delete’
+#   deletes one blog post based on ID in the url
 
 end
